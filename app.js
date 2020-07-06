f9bbcf0c549d57f4f899539bf1cb24b170b8287e
do2bfree <alexey.tivonenko@gmail.com>
2020-07-06 20:24:37 +0300

    Sounds

--- app.js
+++ app.js
@@ -14,7 +14,23 @@ class DrumKit {
 	repeat() {
 		let step = this.index % 8;
 		const activeBars = document.querySelectorAll(`.b${step}`);
-		console.log(step);
+		activeBars.forEach((bar) => {
+			bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+			if (bar.classList.contains("active")) {
+				if (bar.classList.contains("kick-pad")) {
+					this.kickAudio.currentTime = 0;
+					this.kickAudio.play();
+				}
+				if (bar.classList.contains("snare-pad")) {
+					this.snareAudio.currentTime = 0;
+					this.snareAudio.play();
+				}
+				if (bar.classList.contains("hihat-pad")) {
+					this.hihatAudio.currentTime = 0;
+					this.hihatAudio.play();
+				}
+			}
+		});
 		this.index++;
 	}
 	start() {
@@ -29,6 +45,9 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach((pad) => {
 	pad.addEventListener("click", drumKit.activePad);
+	pad.addEventListener("animationend", function () {
+		this.style.animation = "";
+	});
 });
 
 drumKit.playBtn.addEventListener("click", function () {
